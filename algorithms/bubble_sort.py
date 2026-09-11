8718f1044009e4930092ebc1966c5eac88e2222b
Giovani H. <113646706+1N-i@users.noreply.github.com>
2026-09-11 00:27:32 +0000

    beginning

---
+++ algorithms/bubble_sort.py
@@ -0,0 +1,11 @@
+def bubbleSort(nums):
+    len_nums = len(nums)
+    for i in range(len_nums):
+        for j in range(len_nums - i - 1):
+            if nums[j] > nums[j + 1]:
+                nums[j], nums[j + 1] = nums[j + 1], nums[j]
+
+    return nums
+
+if __name__ == "__main__":
+    print(bubbleSort([5, 4, 8, 1, 3, 2, 9, 6, 7]))
---
+++ main.py
@@ -0,0 +1,24 @@
+from algorithms.i_cant_believe_it_can_sort import iCantBelieveItCanSort
+from algorithms.bubble_sort import bubbleSort
+
+import time #Calculate the time of each algorithm
+
+#Functions list
+algorithms = [iCantBelieveItCanSort,
+              bubbleSort
+              ]
+
+#Same list for every algorithm
+main_nums = [5, 4, 8, 1, 3, 2, 9, 6, 7]
+
+print("Algorithms                |    Time (microseconds)| Result")
+for algo in algorithms:
+    nums = main_nums.copy()
+
+    start = time.perf_counter()
+    result = algo(nums)
+    end = time.perf_counter()
+
+    run_time = end - start
+
+    print(f"{algo.__name__:<25} | {run_time * 1000000:>8.2f} microseconds | {nums}")
