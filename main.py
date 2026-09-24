1e95432a5e1e4b6405820473be106818fc1d2f18
1N-i <113646706+1N-i@users.noreply.github.com>
2026-09-23 22:16:27 -0300

    sort preview added

--- main.py
+++ main.py
@@ -13,8 +13,20 @@ sorter = bubbleSortGenerator(nums)
 
 running = True
 while running:
-    for event in pygame.event.get():
+    for event in pygame.event.get(): #checks if the window should close
         if event.type == pygame.QUIT:
             running = False
 
+    surface.fill((30, 30, 30))
     step = next(sorter, None)
+    if step is not None:
+        nums, id1, id2 = step
+        renderer(surface, nums, id1, id2)
+    else:
+        #Future animation for when it's sorted
+        renderer(surface, nums)
+
+    pygame.display.flip()
+    clock.tick(180) #Bigger value = Faster
+
+pygame.quit()
