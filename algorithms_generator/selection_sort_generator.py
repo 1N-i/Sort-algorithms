a98c4ffe91f0aac53c6efde256902451d310f5be
1N-i <113646706+1N-i@users.noreply.github.com>
2026-09-25 12:02:56 -0300

    selection_sort_generator added

--- algorithms_generator/selection_sort_generator.py
+++ algorithms_generator/selection_sort_generator.py
@@ -1,15 +1,18 @@
-def selectionSort(nums):
+def selectionSortGenerator(nums):
     len_nums = len(nums)
-
     for i in range(len_nums):
         min_i = i
         for j in range(i + 1, len_nums):
+            yield nums, j, min_i
             if nums[j] < nums[min_i]:
                 min_i = j
 
         nums[i], nums[min_i] = nums[min_i], nums[i]
+        yield nums, i, min_i
 
     return nums
 
 if __name__ == "__main__":
-    print(selectionSort([5, 4, 8, -1, -3, -2, 9, 6, 7]))
+    nums = [5, 4, 8, -1, -3, -2, 9, 6, 7]
+    for state, i1, i2 in selectionSortGenerator(nums):
+        print(f"Idx {i1} and {i2}: {state}")
--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from algorithms_generator.i_cant_believe_it_can_sort_generator import iCantBelie
 #from algorithms_generator.python_sort import pythonSort
 #from algorithms_generator.quick_sort import quickSort
 #from algorithms_generator.radix_sort import radixSort
-#from algorithms_generator.selection_sort import selectionSort
+from algorithms_generator.selection_sort_generator import selectionSortGenerator
 #from algorithms_generator.shell_sort import shellSort
 
 from utils.data_generator import generateData
@@ -25,21 +25,21 @@ algorithms = [                      #clock.tick(60) value
     bubbleSortGenerator,            #180
     bucketSortGenerator,            #30
     countingSortGenerator,          #45
-    #heapSortGenerator,             #
+    #heapSortGenerator,              #
     iCantBelieveItCanSortGenerator, #480
-    #insertionSortGenerator,        #
-    #mergeSortGenerator,            #
-    #pythonSortGenerator,           #
-    #quickSortGenerator,            #
-    #radixSortGenerator,            #
-    #selectionSortGenerator,        #
-    #shellSortGenerator             #
+    #insertionSortGenerator,         #
+    #mergeSortGenerator,             #
+    #pythonSortGenerator,            #
+    #quickSortGenerator,             #
+    #radixSortGenerator,             #
+    selectionSortGenerator,         #180
+    #shellSortGenerator              #
 ]
 
 
 #("already_sorted", "reverse_sorted", "totally_random", "nearly_sorted")
 nums = generateData(100, "totally_random", True) #(size, style, allow_negatives)
-algo = countingSortGenerator(nums)
+algo = selectionSortGenerator(nums)
 green_id = -1
 
 running = True
@@ -53,9 +53,9 @@ while running:
     if step is not None:
         nums, id1, id2 = step
         renderer(surface, nums, id1, id2)
-        clock.tick(45) #Bigger value = Faster
+        clock.tick(180) #Bigger value = Faster
     else:
-        clock.tick(120)
+        clock.tick(60) #Speed of the sorted animation
         if green_id < len(nums):
             green_id += 1
         renderer(surface, nums, None, None, green_id)
